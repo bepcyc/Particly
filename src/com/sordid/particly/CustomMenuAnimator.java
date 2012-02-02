157f6c813033f887cb7d011e1007df1ae5287b66
Viacheslav Rodionov <vrodionov@luxoft.com>
2012-02-02 16:24:50 +0400

    mavenizing project

--- src/com/sordid/particly/CustomMenuAnimator.java
+++ src/com/sordid/particly/CustomMenuAnimator.java
@@ -1,15 +1,14 @@
 package com.sordid.particly;
 
-import java.util.ArrayList;
-
+import com.sordid.particly.CustomMenuScene.Break;
+import com.sordid.particly.CustomMenuScene.MenuContainer;
 import org.anddev.andengine.entity.IEntity;
 import org.anddev.andengine.entity.modifier.AlphaModifier;
 import org.anddev.andengine.entity.scene.menu.animator.BaseMenuAnimator;
 import org.anddev.andengine.entity.scene.menu.item.IMenuItem;
 import org.anddev.andengine.entity.shape.IShape;
 
-import com.sordid.particly.CustomMenuScene.Break;
-import com.sordid.particly.CustomMenuScene.MenuContainer;
+import java.util.ArrayList;
 
 
 /**
@@ -17,130 +16,132 @@ import com.sordid.particly.CustomMenuScene.MenuContainer;
  * @date 06.10.2011
  */
 public class CustomMenuAnimator extends BaseMenuAnimator {
-        // ===========================================================
-        // Constants
-        // ===========================================================
+    // ===========================================================
+    // Constants
+    // ===========================================================
 
-        private static final float ALPHA_FROM = 0.0f;
-        private static final float ALPHA_TO = 1.0f;
-        private final float mSpacing;
+    private static final float ALPHA_FROM = 0.0f;
+    private static final float ALPHA_TO = 1.0f;
+    private final float mSpacing;
 
-        // ===========================================================
-        // Fields
-        // ===========================================================
+    // ===========================================================
+    // Fields
+    // ===========================================================
 
-        // ===========================================================
-        // Constructors
-        // ===========================================================
+    // ===========================================================
+    // Constructors
+    // ===========================================================
 
-        public CustomMenuAnimator() {
-        	mSpacing = 2.0f;
+    public CustomMenuAnimator() {
+        mSpacing = 2.0f;
+    }
+
+    public CustomMenuAnimator(float pSpacing) {
+        mSpacing = pSpacing;
+    }
+
+    // ===========================================================
+    // Getter & Setter
+    // ===========================================================
+
+    // ===========================================================
+    // Methods for/from SuperClass/Interfaces
+    // ===========================================================
+
+    // ===========================================================
+    // Methods
+    // ===========================================================
+
+    public void buildAnimations(final CustomMenuScene.MenuContainer container) {
+        for (Object o : container.mEntities) {
+            final String name = o.getClass().getSimpleName();
+            if (name.equals("Sprite")
+                    || name.equals("AnimatedSprite")
+                    || name.equals("SpriteMenuItem")
+                    || name.equals("AnimatedSpriteMenuItem")
+                    || name.equals("Text")
+                    ) {
+                if (!((IEntity) o).isVisible()) continue;
+                ((IEntity) o).registerEntityModifier(new AlphaModifier(DURATION, ALPHA_FROM, ALPHA_TO));
+            } else if (name.equals("MenuContainer")) {
+                buildAnimations((MenuContainer) o);
+            }
         }
-        public CustomMenuAnimator(float pSpacing) {
-        	mSpacing = pSpacing;
+    }
+
+    public void prepareAnimations(final CustomMenuScene.MenuContainer container, final float pCameraWidth, final float pCameraHeight) {
+        ArrayList<BufferedMenuCollection> bufferedCollections = new ArrayList<BufferedMenuCollection>();
+        float bufferedHeight = 0;
+        float currentMaxHeight = 0;
+        bufferedCollections.add(new BufferedMenuCollection());
+        IShape entity;
+        String name;
+        for (final Object o : container.mEntities) {
+            name = o.getClass().getSimpleName();
+            if (name.equals("Sprite")
+                    || name.equals("AnimatedSprite")
+                    || name.equals("SpriteMenuItem")
+                    || name.equals("AnimatedSpriteMenuItem")
+                    || name.equals("Text")
+                    ) {
+                if (!((IEntity) o).isVisible()) continue;
+                entity = (IShape) o;
+                entity.setAlpha(ALPHA_FROM);
+                bufferedCollections.get(bufferedCollections.size() - 1).addObject(new BufferedMenuObject(entity, bufferedHeight));
+                currentMaxHeight = Math.max(currentMaxHeight, entity.getHeight() + this.mSpacing);
+            } else if (name.equals("Break")) {
+                bufferedHeight += currentMaxHeight + ((Break) o).getSpacing();
+                bufferedCollections.add(new BufferedMenuCollection());
+                currentMaxHeight = 0;
+            }
         }
 
-        // ===========================================================
-        // Getter & Setter
-        // ===========================================================
+        float basey = pCameraHeight / 2 - bufferedHeight / 2;
+        for (final BufferedMenuCollection collection : bufferedCollections) {
+            float x = pCameraWidth / 2 - collection.totalWidth / 2;
+            for (final BufferedMenuObject object : collection.objects) {
+                object.entityReference.setPosition(x, basey + object.yOffset);
+                x += object.getWidth();
+            }
+        }
+    }
 
-        // ===========================================================
-        // Methods for/from SuperClass/Interfaces
-        // ===========================================================
-        
-        // ===========================================================
-        // Methods
-        // ===========================================================
+    @Override
+    public void buildAnimations(ArrayList<IMenuItem> arg0, float arg1,
+                                float arg2) {
+    }
 
-		public void buildAnimations(final CustomMenuScene.MenuContainer container) {
-			for(Object o : container.mEntities) {
-				final String name = o.getClass().getSimpleName();
-				if(name.equals("Sprite")
-					|| name.equals("AnimatedSprite")
-					|| name.equals("SpriteMenuItem")
-					|| name.equals("AnimatedSpriteMenuItem")
-					|| name.equals("Text")
-				) {
-					if(!((IEntity)o).isVisible()) continue;
-					((IEntity)o).registerEntityModifier(new AlphaModifier(DURATION, ALPHA_FROM, ALPHA_TO));	
-				} else if(name.equals("MenuContainer")) {
-					buildAnimations((MenuContainer)o);
-				}
-			}
-		}
+    @Override
+    public void prepareAnimations(ArrayList<IMenuItem> arg0, float arg1,
+                                  float arg2) {
+    }
 
-		public void prepareAnimations(final CustomMenuScene.MenuContainer container, final float pCameraWidth, final float pCameraHeight) {
-			ArrayList<BufferedMenuCollection> bufferedCollections = new ArrayList<BufferedMenuCollection>();
-			float bufferedHeight = 0;
-			float currentMaxHeight = 0;
-			bufferedCollections.add(new BufferedMenuCollection());
-			IShape entity;
-			String name;
-			for(final Object o : container.mEntities) {
-				name = o.getClass().getSimpleName();
-				if(name.equals("Sprite")
-					|| name.equals("AnimatedSprite")
-					|| name.equals("SpriteMenuItem")
-					|| name.equals("AnimatedSpriteMenuItem")
-					|| name.equals("Text")
-				) {
-					if(!((IEntity)o).isVisible()) continue;
-					entity = (IShape)o;
-					entity.setAlpha(ALPHA_FROM);
-					bufferedCollections.get(bufferedCollections.size()-1).addObject(new BufferedMenuObject(entity, bufferedHeight));
-					currentMaxHeight = Math.max(currentMaxHeight, entity.getHeight() + this.mSpacing);
-				}
-				else if(name.equals("Break")) {
-					bufferedHeight += currentMaxHeight + ((Break)o).getSpacing();
-					bufferedCollections.add(new BufferedMenuCollection());
-					currentMaxHeight = 0;
-				}
-			}	
-			
-			float basey = pCameraHeight / 2 - bufferedHeight / 2;
-			for(final BufferedMenuCollection collection : bufferedCollections) {
-				float x = pCameraWidth / 2 - collection.totalWidth / 2;
-				for(final BufferedMenuObject object : collection.objects) {
-					object.entityReference.setPosition(x, basey + object.yOffset);
-					x += object.getWidth();
-				}
-			}
-		}
+    public class BufferedMenuCollection {
+        private final ArrayList<BufferedMenuObject> objects = new ArrayList<BufferedMenuObject>();
+        private float totalWidth = 0;
 
-		@Override
-		public void buildAnimations(ArrayList<IMenuItem> arg0, float arg1,
-				float arg2) {}
+        public void addObject(BufferedMenuObject pObject) {
+            objects.add(pObject);
+            totalWidth += pObject.getWidth();
+            if (objects.size() > 1) {
+                totalWidth += CustomMenuAnimator.this.mSpacing;
+            }
+        }
+    }
 
-		@Override
-		public void prepareAnimations(ArrayList<IMenuItem> arg0, float arg1,
-				float arg2) {}
-		
-		public class BufferedMenuCollection {
-			private ArrayList<BufferedMenuObject> objects = new ArrayList<BufferedMenuObject>();
-			private float totalWidth = 0;
-			
-			public void addObject(BufferedMenuObject pObject) {
-				objects.add(pObject);
-				totalWidth += pObject.getWidth();
-				if(objects.size() > 1) {
-					totalWidth += CustomMenuAnimator.this.mSpacing;
-				}
-			}
-		}
-		
-		public class BufferedMenuObject{
-			private final IShape entityReference;
-			private final float yOffset;
-			
-			public BufferedMenuObject(final IShape pEntity, final float pYOffset) {
-				entityReference = pEntity;
-				yOffset = pYOffset;
-			}
-			
-			public float getWidth() {
-				return entityReference.getWidth() + CustomMenuAnimator.this.mSpacing;
-			}
-		}
-        
-        // ===========================================================
+    public class BufferedMenuObject {
+        private final IShape entityReference;
+        private final float yOffset;
+
+        public BufferedMenuObject(final IShape pEntity, final float pYOffset) {
+            entityReference = pEntity;
+            yOffset = pYOffset;
+        }
+
+        public float getWidth() {
+            return entityReference.getWidth() + CustomMenuAnimator.this.mSpacing;
+        }
+    }
+
+    // ===========================================================
 }
--- src/com/sordid/particly/CustomMenuScene.java
+++ src/com/sordid/particly/CustomMenuScene.java
@@ -1,8 +1,6 @@
 package com.sordid.particly;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
+import android.view.MotionEvent;
 import org.anddev.andengine.engine.camera.Camera;
 import org.anddev.andengine.entity.scene.Scene;
 import org.anddev.andengine.entity.scene.menu.MenuScene;
@@ -11,207 +9,218 @@ import org.anddev.andengine.entity.shape.IShape;
 import org.anddev.andengine.entity.shape.Shape;
 import org.anddev.andengine.input.touch.TouchEvent;
 
-import android.view.MotionEvent;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * @author Methodin
  * @date 06.10.2011
  */
 public class CustomMenuScene extends MenuScene {
-	
-	// ===========================================================
-	// Fields
-	// ===========================================================
-	
-	protected IOnMenuItemClickListener mOnMenuItemClickListener;
 
-	protected CustomMenuAnimator mMenuAnimator = null;
+    // ===========================================================
+    // Fields
+    // ===========================================================
 
-	protected IMenuItem mSelectedMenuItem;
-	
-	protected MenuContainer currentContainer = new MenuContainer();
-	
-	protected HashMap<String, IShape> mItems = new HashMap<String, IShape>();
-	
-	// ===========================================================
-	// Constructors
-	// ===========================================================
+    protected IOnMenuItemClickListener mOnMenuItemClickListener;
 
-	public CustomMenuScene() {
-		this(null, null);
-	}
+    protected CustomMenuAnimator mMenuAnimator = null;
 
-	public CustomMenuScene(final IOnMenuItemClickListener pOnMenuItemClickListener) {
-		this(null, pOnMenuItemClickListener);
-	}
+    protected IMenuItem mSelectedMenuItem;
 
-	public CustomMenuScene(final Camera pCamera) {
-		this(pCamera, null);
-	}
+    protected MenuContainer currentContainer = new MenuContainer();
 
-	public CustomMenuScene(final Camera pCamera, final IOnMenuItemClickListener pOnMenuItemClickListener) {
-		super(pCamera);
-		this.mOnMenuItemClickListener = pOnMenuItemClickListener;
-		this.setOnSceneTouchListener(this);
-		this.setOnAreaTouchListener(this);
-	}	
-	
-	// ===========================================================
-	// Getter & Setter
-	// ===========================================================
-	
-	public IOnMenuItemClickListener getOnMenuItemClickListener() {
-		return this.mOnMenuItemClickListener;
-	}
+    protected final HashMap<String, IShape> mItems = new HashMap<String, IShape>();
 
-	public void setOnMenuItemClickListener(final IOnMenuItemClickListener pOnMenuItemClickListener) {
-		this.mOnMenuItemClickListener = pOnMenuItemClickListener;
-	}
+    // ===========================================================
+    // Constructors
+    // ===========================================================
 
-	@Override
-	public MenuScene getChildScene() {
-		return (MenuScene)super.getChildScene();
-	}
+    public CustomMenuScene() {
+        this(null, null);
+    }
 
-	@Override
-	public void setChildScene(final Scene pChildScene, final boolean pModalDraw, final boolean pModalUpdate, final boolean pModalTouch) throws IllegalArgumentException {
-		if(pChildScene instanceof MenuScene) {
-			super.setChildScene(pChildScene, pModalDraw, pModalUpdate, pModalTouch);
-		} else {
-			throw new IllegalArgumentException("MenuScene accepts only MenuScenes as a ChildScene.");
-		}
-	}
+    public CustomMenuScene(final IOnMenuItemClickListener pOnMenuItemClickListener) {
+        this(null, pOnMenuItemClickListener);
+    }
 
-	@Override
-	public void clearChildScene() {
-		if(this.getChildScene() != null) {
-			this.getChildScene().reset();
-			super.clearChildScene();
-		}
-	}
+    public CustomMenuScene(final Camera pCamera) {
+        this(pCamera, null);
+    }
 
-	public void setMenuAnimator(final CustomMenuAnimator pMenuAnimator) {
-		this.mMenuAnimator = pMenuAnimator;
-	}	
-	
-	// ===========================================================
-	// Methods for/from SuperClass/Interfaces
-	// ===========================================================	
-	
-	@Override
-	public void buildAnimations() {
-		this.prepareAnimations();
-		this.mMenuAnimator.buildAnimations(this.currentContainer);
-	}
+    public CustomMenuScene(final Camera pCamera, final IOnMenuItemClickListener pOnMenuItemClickListener) {
+        super(pCamera);
+        this.mOnMenuItemClickListener = pOnMenuItemClickListener;
+        this.setOnSceneTouchListener(this);
+        this.setOnAreaTouchListener(this);
+    }
 
-	@Override
-	public void prepareAnimations() {
-		final float cameraHeight = this.mCamera.getHeight();
-		final float cameraWidth = this.mCamera.getWidth();
-		this.mMenuAnimator.prepareAnimations(this.currentContainer, cameraWidth, cameraHeight);
-	}
-	
-	@Override
+    // ===========================================================
+    // Getter & Setter
+    // ===========================================================
+
+    public IOnMenuItemClickListener getOnMenuItemClickListener() {
+        return this.mOnMenuItemClickListener;
+    }
+
+    public void setOnMenuItemClickListener(final IOnMenuItemClickListener pOnMenuItemClickListener) {
+        this.mOnMenuItemClickListener = pOnMenuItemClickListener;
+    }
+
+    @Override
+    public MenuScene getChildScene() {
+        return super.getChildScene();
+    }
+
+    @Override
+    public void setChildScene(final Scene pChildScene, final boolean pModalDraw, final boolean pModalUpdate, final boolean pModalTouch) throws IllegalArgumentException {
+        if (pChildScene instanceof MenuScene) {
+            super.setChildScene(pChildScene, pModalDraw, pModalUpdate, pModalTouch);
+        } else {
+            throw new IllegalArgumentException("MenuScene accepts only MenuScenes as a ChildScene.");
+        }
+    }
+
+    @Override
+    public void clearChildScene() {
+        if (this.getChildScene() != null) {
+            this.getChildScene().reset();
+            super.clearChildScene();
+        }
+    }
+
+    public void setMenuAnimator(final CustomMenuAnimator pMenuAnimator) {
+        this.mMenuAnimator = pMenuAnimator;
+    }
+
+    // ===========================================================
+    // Methods for/from SuperClass/Interfaces
+    // ===========================================================
+
+    @Override
+    public void buildAnimations() {
+        this.prepareAnimations();
+        this.mMenuAnimator.buildAnimations(this.currentContainer);
+    }
+
+    @Override
+    public void prepareAnimations() {
+        final float cameraHeight = this.mCamera.getHeight();
+        final float cameraWidth = this.mCamera.getWidth();
+        this.mMenuAnimator.prepareAnimations(this.currentContainer, cameraWidth, cameraHeight);
+    }
+
+    @Override
     public boolean onAreaTouched(final TouchEvent pSceneTouchEvent, final ITouchArea pTouchArea, final float pTouchAreaLocalX, final float pTouchAreaLocalY) {
-		final IMenuItem menuItem = ((IMenuItem)pTouchArea);
-		switch(pSceneTouchEvent.getAction()) {
-			case MotionEvent.ACTION_DOWN:
-			case MotionEvent.ACTION_MOVE:
-				if(this.mSelectedMenuItem != null && this.mSelectedMenuItem != menuItem) {
-					this.mSelectedMenuItem.onUnselected();
-				}
-				this.mSelectedMenuItem = menuItem;
-				this.mSelectedMenuItem.onSelected();
-				break;
-			case MotionEvent.ACTION_UP:
-				if(this.mOnMenuItemClickListener != null) {
-					final boolean handled = this.mOnMenuItemClickListener.onMenuItemClicked(this, menuItem, pTouchAreaLocalX, pTouchAreaLocalY);
-					menuItem.onUnselected();
-					this.mSelectedMenuItem = null;
-					return handled;
-				}
-				break;
-			case MotionEvent.ACTION_CANCEL:
-				menuItem.onUnselected();
-				this.mSelectedMenuItem = null;
-				break;
-		}
-		return true;
-	}	
-	
-	// ===========================================================
-	// Methods
-	// ===========================================================
-	public CustomMenuScene add(Shape pEntity) {
-		final String name = pEntity.getClass().getSimpleName();
-		if(name.equals("SpriteMenuItem")
-			|| name.equals("AnimatedSpriteMenuItem")
-			|| name.equals("Text")
-			|| name.equals("Sprite")
-			|| name.equals("AnimatedSprite")
-		) {
-			this.getFirstChild().attachChild((IShape)pEntity);
-			if(name.equals("SpriteMenuItem") || name.equals("AnimatedSpriteMenuItem")) {
-				this.registerTouchArea((IMenuItem)pEntity);
-			}
-		}
-		currentContainer.add(pEntity);
-		return this;
-	}
-	public CustomMenuScene add(String identifier, Shape pEntity) {
-		mItems.put(identifier, pEntity);
-		return this.add(pEntity);
-	}
-	
-	public CustomMenuScene br() {
-		currentContainer.add(new Break());
-		return this;
-	}
-	public CustomMenuScene br(float spacing) {
-		currentContainer.add(new Break(spacing));
-		return this;
-	}
-	
-	public CustomMenuScene container() {
-		final MenuContainer newContainer = new MenuContainer(currentContainer);
-		currentContainer.add(newContainer);
-		currentContainer = newContainer;
-		return this;
-	}
-	public CustomMenuScene end() {
-		currentContainer = currentContainer.getParent();
-		return this;
-	}	
+        final IMenuItem menuItem = ((IMenuItem) pTouchArea);
+        switch (pSceneTouchEvent.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:
+                if (this.mSelectedMenuItem != null && this.mSelectedMenuItem != menuItem) {
+                    this.mSelectedMenuItem.onUnselected();
+                }
+                this.mSelectedMenuItem = menuItem;
+                this.mSelectedMenuItem.onSelected();
+                break;
+            case MotionEvent.ACTION_UP:
+                if (this.mOnMenuItemClickListener != null) {
+                    final boolean handled = this.mOnMenuItemClickListener.onMenuItemClicked(this, menuItem, pTouchAreaLocalX, pTouchAreaLocalY);
+                    menuItem.onUnselected();
+                    this.mSelectedMenuItem = null;
+                    return handled;
+                }
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                menuItem.onUnselected();
+                this.mSelectedMenuItem = null;
+                break;
+        }
+        return true;
+    }
 
-	// ===========================================================
-	// Classes
-	// ==========================================================
-	
-	public class MenuContainer {
-		MenuContainer mParent = null;
-		ArrayList<Object> mEntities = new ArrayList<Object>();
+    // ===========================================================
+    // Methods
+    // ===========================================================
+    public CustomMenuScene add(Shape pEntity) {
+        final String name = pEntity.getClass().getSimpleName();
+        if (name.equals("SpriteMenuItem")
+                || name.equals("AnimatedSpriteMenuItem")
+                || name.equals("Text")
+                || name.equals("Sprite")
+                || name.equals("AnimatedSprite")
+                ) {
+            this.getFirstChild().attachChild(pEntity);
+            if (name.equals("SpriteMenuItem") || name.equals("AnimatedSpriteMenuItem")) {
+                this.registerTouchArea(pEntity);
+            }
+        }
+        currentContainer.add(pEntity);
+        return this;
+    }
 
-		public MenuContainer() {}
-		public MenuContainer(MenuContainer pParent) {
-			mParent = pParent;
-		}
-		
-		public MenuContainer getParent() {
-			return mParent;
-		}
-	
-		public void add(Object item) {
-			mEntities.add(item);
-		}
-	}
-	public class Break{
-		private float mSpacing = 0;
-		public Break(){}
-		public Break(float pSpacing){
-			mSpacing = pSpacing;
-		}
-		public float getSpacing() {
-			return mSpacing;
-		}
-	}
+    public CustomMenuScene add(String identifier, Shape pEntity) {
+        mItems.put(identifier, pEntity);
+        return this.add(pEntity);
+    }
+
+    public CustomMenuScene br() {
+        currentContainer.add(new Break());
+        return this;
+    }
+
+    public CustomMenuScene br(float spacing) {
+        currentContainer.add(new Break(spacing));
+        return this;
+    }
+
+    public CustomMenuScene container() {
+        final MenuContainer newContainer = new MenuContainer(currentContainer);
+        currentContainer.add(newContainer);
+        currentContainer = newContainer;
+        return this;
+    }
+
+    public CustomMenuScene end() {
+        currentContainer = currentContainer.getParent();
+        return this;
+    }
+
+    // ===========================================================
+    // Classes
+    // ==========================================================
+
+    public class MenuContainer {
+        MenuContainer mParent = null;
+        final ArrayList<Object> mEntities = new ArrayList<Object>();
+
+        public MenuContainer() {
+        }
+
+        public MenuContainer(MenuContainer pParent) {
+            mParent = pParent;
+        }
+
+        public MenuContainer getParent() {
+            return mParent;
+        }
+
+        public void add(Object item) {
+            mEntities.add(item);
+        }
+    }
+
+    public class Break {
+        private float mSpacing = 0;
+
+        public Break() {
+        }
+
+        public Break(float pSpacing) {
+            mSpacing = pSpacing;
+        }
+
+        public float getSpacing() {
+            return mSpacing;
+        }
+    }
 }
